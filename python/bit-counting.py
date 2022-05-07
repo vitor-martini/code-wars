b126f5c579960b561b2803604103b90038f9f0fb
vitor martini <vitor-martini@outlook.com>
2022-05-07 19:07:34 -0300

    bit-counting

--- python/bit-counting.py
+++ python/bit-counting.py
@@ -1,7 +1,7 @@
 # https://www.codewars.com/kata/526571aae218b8ee490006f4/train/python
 
 def count_bits(n):
-    result = str(bin(n)).replace('0b', '').count('1')
+    result = bin(n).count('1')
     return result
     
 def main():
